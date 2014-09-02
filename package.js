260d780877c1ad61bcb67ee4288690ab0f853187
Zoltan Olah <zol@percolatestudio.com>
2014-09-02 13:22:51 -0700

    Packaged for 0.9.0.1

--- package.js
+++ package.js
@@ -1,9 +1,17 @@
 Package.describe({
-  summary: "A momentum plugin for transitioning Iron Router pages"
+  summary: "A momentum plugin for transitioning Iron Router pages",
+  version: "0.0.1",
+  name: "percolate:momentum-iron-router",
+  git: "https://github.com/percolatestudio/momentum-iron-router.git"
 });
 
 Package.on_use(function (api, where) {
-  api.use(['deps', 'iron-router', 'momentum'], 'client');
+  api.versionsFrom('METEOR@0.9.0.1');
+  
+  api.use(['deps', 
+    'iron:router@0.9.1', 
+    'percolate:momentum@0.0.1'], 
+  'client');
   
   api.add_files('momentum-transitioner.js', ['client']);
 });
