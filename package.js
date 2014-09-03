dc52f6cd33a1be83f574b3e84e5ce8028b512fa2
Tom Coleman <tom@thesnail.org>
2014-09-03 16:41:13 +1000

    Getting a version working with 0.9.2-rc0

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  summary: "A momentum plugin for transitioning Iron Router pages",
-  version: "0.0.1",
   name: "percolate:momentum-iron-router",
+  summary: "A momentum plugin for transitioning Iron Router pages",
+  version: "0.0.2-rc0",
   git: "https://github.com/percolatestudio/momentum-iron-router.git"
 });
 
 Package.on_use(function (api, where) {
-  api.versionsFrom('METEOR@0.9.0.1');
+  api.versionsFrom('METEOR@0.9.2-rc0');
   
   api.use(['deps', 
-    'iron:router@0.9.1', 
-    'percolate:momentum@0.0.1'], 
+    'iron:router@0.9.3-rc0', 
+    'percolate:momentum@0.0.3-rc0'], 
   'client');
   
   api.add_files('momentum-transitioner.js', ['client']);
