e11cf2cc7509f3ed93f9721d6df4e364a2266077
Tom Coleman <tom@thesnail.org>
2014-09-08 15:06:13 +1000

    Revert "Added transition queueing"
    This reverts commit 828bb29ed8bbe207c9d874f9352e2e00bb7ab191.
    
    Conflicts:
    	momentum-transitioner.js

--- momentum-transitioner.js
+++ momentum-transitioner.js
@@ -1,11 +1,4 @@
 var Transitioner = {
-  // the number of elements that are currently mid-transition
-  transitioning: 0,
-  
-  // should we be queuing transitions right now?
-  queueing: false,
-  queuedTransitions: [],
-  
   renders: [],
   start: function() {
     var self = this;
@@ -25,53 +18,12 @@ var Transitioner = {
     });
   },
   
-  // either 
-  //  a) run the fn, and queue any further transitions until it's done
-  //  b) queue the fn
-  runTransition: function(fn) {
-    var self = this;
-    
-    if (self.queueing)
-      return self.queue(fn);
-    
-    // if we finish the last active transition, we can run the queue
-    var done = function() {
-      self.transitioning -= 1;
-      if (self.transitioning === 0)
-        self.runQueue();
-    }
-    
-    self.transitioning += 1;
-    fn(done);
-    
-    // when we run the first transition, wait until the end of the flush cycle
-    //    (so for example if this is the removeElement, the corresponding 
-    //      insertElement runs too)
-    //  after which, we queue all incoming transitions
-    if (self.transitioning === 1) {
-      Deps.afterFlush(function() {
-        self.queueing = (self.transitioning > 0);
-      });
-    }
-  },
-  
-  queue: function(transition) {
-    this.queuedTransitions.push(transition);
-  },
-  
-  runQueue: function() {
-    var self = this;
-    self.queueing = false;
-    _.each(self.queuedTransitions, function(transition) {
-      self.runTransition(transition);
-    });
-    self.queuedTransitions = [];
-  }
 }
 Transitioner.start();
 
 Momentum.registerPlugin('iron-router', function(options) {
   check(options.options, Match.Optional(Function));
+  console.log(options)
   
   var getPluginOptions = function(node) {
     var type = options.options &&
@@ -97,23 +49,15 @@ Momentum.registerPlugin('iron-router', function(options) {
     return plugin(_.omit(pluginOptions, 'with'));
   }
   
-  // XXX: strictly speaking, these functions should *also* take a done
-  //   and merge that with the done that the transitioner's waiting for.
   return {
     insertElement: function(node, next) {
-      Transitioner.runTransition(function(done) {
-        getPlugin(node).insertElement(node, next, done);
-      });
+      getPlugin(node).insertElement(node, next);
     },
     moveElement: function(node, next) {
-      Transitioner.runTransition(function(done) {
-        getPlugin(node).moveElement(node, next, done);
-      });
+      getPlugin(node).moveElement(node, next);
     },
     removeElement: function(node) {
-      Transitioner.runTransition(function(done) {
-        getPlugin(node).removeElement(node, done);
-      });
+      getPlugin(node).removeElement(node);
     },
     // force: true
   }
