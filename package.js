a92b8e7ef5898ab966e9f44de33b4debb0954310
Tom Coleman <tom@thesnail.org>
2014-10-09 15:52:00 +1100

    0.5.0

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: "percolate:momentum-iron-router",
   summary: "A momentum plugin for transitioning Iron Router pages",
-  version: "0.0.2-rc0",
+  version: "0.5.0",
   git: "https://github.com/percolatestudio/momentum-iron-router.git"
 });
 
 Package.on_use(function (api, where) {
-  api.versionsFrom('METEOR@0.9.2-rc0');
+  api.versionsFrom('METEOR@0.9.2');
   
   api.use(['deps', 
-    'iron:router@0.9.3-rc0', 
-    'percolate:momentum@0.0.3-rc1'], 
+    'iron:router@0.9.3', 
+    'percolate:momentum@0.5.0'], 
   'client');
   
-  api.add_files('momentum-transitioner.js', ['client']);
+  api.add_files('momentum-iron-router.js', ['client']);
 });
 
