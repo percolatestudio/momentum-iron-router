f5e3100bb9ef94cabd9b0949bd50f58baf509c97
Zoltan Olah <zol@percolatestudio.com>
2014-11-04 08:38:00 -0800

    Uses latest momentum

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "percolate:momentum-iron-router",
   summary: "A momentum plugin for transitioning Iron Router pages",
-  version: "0.6.0",
+  version: "0.7.0",
   git: "https://github.com/percolatestudio/momentum-iron-router.git"
 });
 
@@ -10,7 +10,7 @@ Package.on_use(function (api, where) {
   
   api.use(['tracker', 
     'iron:router@1.0.0', 
-    'percolate:momentum@0.5.0'], 
+    'percolate:momentum@0.6.0'], 
   'client');
   
   api.add_files('momentum-iron-router.js', ['client']);
